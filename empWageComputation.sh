ccc7dbe6f52d0d1418f52b367b1cb6f911e432be
sravanichinna <sravanidane413.com>
2020-11-14 17:09:41 +0530

    added: employeeDayWiseSalaryInArrayUC logic

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -152,3 +152,48 @@ do
 	totalWorkHours=$(( $totalWorkHours + $workHours ))
 	echo -e "TotalWorkHours: $totalWorkHours\n"
 done
+
+IS_PART_TIME=1
+IS_FULL_TIME=2
+MAX_HRS_IN_MONTH=100
+EMP_RATE_PER_HR=20
+NUM_WORKING_DAYS=20
+totalEmpHrs=0
+totalWorkingDays=0
+totalWorkHours=0
+function calculateDailyWage()
+{
+	empHrs=$1
+	daySalary=$(( $empHrs * $EMP_RATE_PER_HR ))
+	echo $daySalary
+}
+
+function getWorkingHours()
+{
+	case $1 in
+		$IS_FULL_TIME ) empHrs=8
+			;;
+		$IS_PART_TIME ) empHrs=4
+			;;
+		*) empHrs=0
+			;;
+esac
+echo $empHrs
+}
+
+while(( $totalWorkHours < $MAX_HRS_IN_MONTH && $totalWorkingDays < $NUM_WORKING_DAYS ))
+do
+	((totalWorkingDays++))
+	workHours="$( getWorkingHours $(( RANDOM%3 )) )"
+
+	echo "Work hours from function getWorkingHours()::::::::::>>> $workHours "
+	totalWorkHours=$(( $totalWorkHours + $workHours ))
+	echo -e "TotalWorkHours: $totalWorkHours\n"
+	empDailyWage[$totalWorkingDays]="$( calculateDailyWage $workHours )"
+done
+	totalSalary=$(( $totalWorkHours * $EMP_RATE_PER_HR ));
+	echo -e "\n::::::::::::Printing Days from array::::::"
+	echo ${!empDailyWage[@]}
+
+	echo -e "\n::::::::::::Printing salary stored day wise in an array:::::::::::::::"
+	echo ${empDailyWage[@]}
