4525f9e5435b65978bc4ff10753437222cbceb68
sravanichinna <sravanidane413.com>
2020-11-14 15:55:40 +0530

    added: employeeAttendanceUC logic

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 echo "welcome to Employee wage computation"
+
+isPresent=1
+random=$(( RANDOM%2 ))
+echo "Random number: "$random
+if(( $random == $isPresent ))
+then
+echo "Employee is Present!"
+else
+echo "Employee is absent!"
+fi
