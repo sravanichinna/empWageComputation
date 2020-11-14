0586f7b1fa3bb8fb01b14bfc48a11d643c8c9300
sravanichinna <sravanidane413.com>
2020-11-14 16:25:34 +0530

    added: employeeWorkingHoursUC logic

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -46,4 +46,18 @@ echo "Random number: " $random
 	salary=$(( $empHrs * $ EMP_RATE_PER_HR ))
 	echo "salary: $salary"
 
-
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HR=20
+random=$(( RANDOM%3 ))
+echo "Random number: $random"
+case $random in
+		$IS_PART_TIME) empHrs=4
+			;;
+		$IS_FULL_TIME) empHrs=8
+			;;
+		*) empHrs=0
+esac
+echo "Employee hours: $empHrs"
+salary=$(( $empHrs * $EMP_RATE_PER_HR ))
+echo "Salary: $salary"
