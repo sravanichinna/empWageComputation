0c65144dbdba33e63c9b11422af416018072d3cf
sravanichinna <sravanidane413.com>
2020-11-14 16:45:32 +0530

    added: employeeSalaryMAX_HRS_IN_MONTHUC logic

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -83,3 +83,34 @@ esac
 done
 
 echo "Total Salary: $totalSalary"
+
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HR=20
+MAX_HRS_IN_MONTH=10
+NUM_WORKING_DAYS=20
+totalEmpHrs=0
+totalWorkingDays=0
+
+while(( $totalEmpHrs < $MAX_HRS_IN_MONTH && $totalWorkingDays < $NUM_WORKING_DAYS ))
+do
+	((totalWorkingDays++))
+	echo "Total working day: $totalWorkingDays"
+	random=$(( RANDOM%3 ))
+
+case $random in
+
+	$IS_PART_TIME) empHrs=4
+		;;
+	$IS_FULL_TIME) empHrs=8
+		;;
+	*) empHrs=0
+
+esac
+
+totalEmpHrs=$(( $totalEmpHrs + $empHrs ))
+echo "Total Employee Hours: $totalEmpHrs"
+
+done
+totalSalary=$(( $totalEmpHrs * $EMP_RATE_PER_HR))
+echo "Total Salary: $totalSalary"
