cb0bcb6143723e364b2c78e403761710ebfa79df
sravanichinna <sravanidane413.com>
2020-11-14 16:05:57 +0530

    added: employeeSalaryUC logic

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,9 +4,24 @@ echo "welcome to Employee wage computation"
 isPresent=1
 random=$(( RANDOM%2 ))
 echo "Random number: "$random
-if(( $random == $isPresent ))
-then
-echo "Employee is Present!"
-else
-echo "Employee is absent!"
-fi
+	if(( $random == $isPresent ))
+	then
+		echo "Employee is Present!"
+	else
+		echo "Employee is absent!"
+	fi
+isPresent=1
+random=$(( RANDOM%2 ))
+echo "Random number: "$random
+	if(( $random == $isPresent ))
+	then
+		echo "Employee is Present!"
+		empHrs=8
+		ratePerHour=20
+		salary=$(( $empHrs * $ratePerHour))
+	else
+		echo "Employee is absent!"
+		salary=0
+	fi
+
+echo "Salary: $salary"
